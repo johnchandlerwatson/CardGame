c3eb688d729e540d23e78d7bb45bdb04c264372e
Nate Whitesell <njwhites2012@gmail.com>
2018-04-15 17:43:31 -0500

    start server script fix

--- build/dev-server.js
+++ build/dev-server.js
@@ -62,8 +62,8 @@ app.use(hotMiddleware)
 
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-//app.use(staticPath, express.static('./static'))
-app.use(serveStatic(path.join(__dirname + "/wwwroot")));
+app.use(staticPath, express.static('./static'))
+//app.use(serveStatic(path.join(__dirname + "/wwwroot")));
 
 var uri = 'http://localhost:' + port
 
--- server.js
+++ server.js
@@ -70,7 +70,8 @@ app.use(hotMiddleware)
 
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+app.use(staticPath, express.static('./wwwroot/static'))
+//app.use(serveStatic(__dirname + "/wwwroot"));
 
 var uri = 'http://localhost:' + port
 
