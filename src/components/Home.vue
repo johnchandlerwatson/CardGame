d4d70978f0012cb37582050d28628f8209d5bdc3
Nate Whitesell <njwhites2012@gmail.com>
2018-04-15 13:24:15 -0500

    Heroku is annoying

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <template id="home">
   <div id="app">
     <img src="./../assets/logo.png">
