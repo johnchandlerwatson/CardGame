119779485d1f34871ced2bd21f0921d71960da99
Nate Whitesell <njwhites2012@gmail.com>
2018-04-15 10:03:38 -0500

    heroku stuff

--- Program.cs
+++ Program.cs
@@ -14,19 +14,7 @@ namespace Vue
     {
         public static void Main(string[] args)
         {
-            var url = $"http://*:{Environment.GetEnvironmentVariable("PORT")}/";
-
-            Console.WriteLine($"Using Url: {url}");
-
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseUrls(url)
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
